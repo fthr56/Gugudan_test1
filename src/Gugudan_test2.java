415c8e3a9aa3519ed7b50e01c2ed14c07eb48a00
LHS <LHS@DESKTOP-A48G9NV>
2017-06-27 19:16:29 +0900

    최종요구 사항2: "8,7"을 입력하면 2*1,...2*7,...,8*7 구현

--- src/Gugudan_test2.java
+++ src/Gugudan_test2.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 
 public class Gugudan_test2 {
 	public static void main(String[] args){
-		System.out.println("test2");
+		
+		System.out.println("n,m 형식으로 값을 입력하시오.(n과 m은 숫자)");
+		Scanner scanner = new Scanner(System.in);
+		String inputValue = scanner.nextLine();
+		
+		String[] splitedValue = inputValue.split(",");
+		
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		
+		for(int dan=2; dan<=first; dan++){
+			for(int number = 1; number <= second; number++){
+				System.out.println(dan + "*" + number + " = " + dan*number);
+			}
+		}
 	}
 }
